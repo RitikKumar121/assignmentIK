fd48ec94f81e6ed2eb5916589ce31e9b42692d3a
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2023-07-10 21:36:54 +0530

    commit

--- blog/blogpage/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from blogpage.models import BlogPostTable
-
-
-@admin.register(BlogPostTable)
-class PostAdmin(admin.ModelAdmin):
-    class Media:
-        js = ('tinyInject.js',)
--- blog/blogpage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from blogpage import views
-urlpatterns = [
-    path('',views.blogPage),
-    path('<str:author>', views.blogPost, name="blogPost"),
-]
